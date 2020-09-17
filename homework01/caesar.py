97a4b7edaac8861f2c557ef40531fcc19a5c6c9d
Timofeev Nik <timofeevnik41@gmail.com>
2020-09-17 10:59:42 +0300

    fixed readability

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,12 +16,12 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ''
     for letter in plaintext:
-        if (65 <= ord(letter) + 3 <= 90) or (97 <= ord(letter) + 3 <= 122):
+        if (65 <= ord(letter) + shift <= 90) or (97 <= ord(letter) + shift <= 122):
             ciphertext += chr(ord(letter) + 3)
         elif (ord(letter) < 65) or (90 < ord(letter) < 97) or (ord(letter) > 122):
             ciphertext += letter
         else:
-            ciphertext += chr(ord(letter) - 23)
+            ciphertext += chr(ord(letter) - (26 - shift))
             
     return ciphertext
 
@@ -41,12 +41,12 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     for letter in ciphertext:
-        if (65 <= ord(letter) - 3 <= 90) or (97 <= ord(letter) - 3 <= 122):
-            plaintext += chr(ord(letter) - 3)
+        if (65 <= ord(letter) - shift <= 90) or (97 <= ord(letter) - shift <= 122):
+            plaintext += chr(ord(letter) - shift)
         elif (ord(letter) < 65) or (90 < ord(letter) < 97) or (ord(letter) > 122):
             plaintext += letter
         else:
-            plaintext += chr(ord(letter) + 23) ##- 26 + 3
+            plaintext += chr(ord(letter) + (26 - shift))
     return plaintext
 
 
@@ -56,4 +56,8 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
     best_shift = 0
     # PUT YOUR CODE HERE
+    ##for letter in ciphertext:
+        ##
     return best_shift
+
+print(decrypt_caesar("Sbwkrq3.6"))
