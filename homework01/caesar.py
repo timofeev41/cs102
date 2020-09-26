4bcc35ccd318bc1d183be19cc7d16bdecd726035
Timofeev Nik <timofeevnik41@gmail.com>
2020-09-26 11:22:30 +0300

    fixed decrypt_caesar and encrypt_caesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -14,15 +14,20 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = ''
+    ciphertext = ""
+
     for letter in plaintext:
-        if (65 <= ord(letter) + shift <= 90) or (97 <= ord(letter) + shift <= 122):
-            ciphertext += chr(ord(letter) + 3)
-        elif (ord(letter) < 65) or (90 < ord(letter) < 97) or (ord(letter) > 122):
+        if (ord("A") <= ord(letter) + shift <= ord("Z")) or (
+            ord("a") <= ord(letter) + shift <= ord("z")
+        ):
+            ciphertext += chr(ord(letter) + shift)
+        elif (
+            (ord(letter) < ord("A")) or ("Z" < ord(letter) < "a") or (ord(letter) > "z")
+        ):
             ciphertext += letter
         else:
             ciphertext += chr(ord(letter) - (26 - shift))
-            
+
     return ciphertext
 
 
@@ -40,24 +45,27 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
+
     for letter in ciphertext:
-        if (65 <= ord(letter) - shift <= 90) or (97 <= ord(letter) - shift <= 122):
+        if (ord("A") <= ord(letter) - shift <= ord("Z")) or (
+            ord("a") <= ord(letter) - shift <= ord("z")
+        ):
             plaintext += chr(ord(letter) - shift)
-        elif (ord(letter) < 65) or (90 < ord(letter) < 97) or (ord(letter) > 122):
+        elif (
+            (ord(letter) < ord("A")) or ("Z" < ord(letter) < "a") or (ord(letter) > "z")
+        ):
             plaintext += letter
         else:
             plaintext += chr(ord(letter) + (26 - shift))
     return plaintext
 
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    ##for letter in ciphertext:
-        ##
-    return best_shift
-
-print(decrypt_caesar("Sbwkrq3.6"))
+# def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+#     """
+#     Brute force breaking a Caesar cipher.
+#     """
+#     best_shift = 0
+#     # PUT YOUR CODE HERE
+#     ##for letter in ciphertext:
+#     ##
+#     return best_shift
