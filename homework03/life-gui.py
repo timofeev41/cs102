9f6b6f9eea869f1a61f9d56a71649e8ed5833f40
Timofeev Nik <timofeevnik41@gmail.com>
2020-11-08 21:36:45 +0300

    Добавлены аргументы командной строки для gui версии

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -1,3 +1,4 @@
+import argparse
 import pygame
 from life import GameOfLife
 from pygame.locals import *
@@ -29,11 +30,13 @@ class GUI(UI):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
+        self.primary_color = arguments.primary_color
+        self.secondary_color = arguments.secondary_color
         for pos_x, row in enumerate(self.life.curr_generation):
             for pos_y, col in enumerate(row):
-                color = pygame.Color("white")
+                color = self.secondary_color
                 if col:
-                    color = pygame.Color("green")
+                    color = self.primary_color
                 pygame.draw.rect(
                     self.screen,
                     color,
@@ -46,26 +49,74 @@ class GUI(UI):
                 )
 
     def run(self) -> None:
+        """
+        Запустить игру.
+
+        Управление:
+        Пробел - Пауза.
+        Esc - Выход из игры.
+        Нажатие ЛКМ - "закрасить" клетку.
+        """
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
         self.grid = self.life.create_grid(True)
         running = True
+        paused = False
         while running:
             if not self.life.is_changing or self.life.is_max_generations_exceeded:
                 running = False
             for event in pygame.event.get():
                 if event.type == QUIT:  # type: ignore
                     running = False
+                if event.type == KEYDOWN:  # type: ignore
+                    if event.key == K_ESCAPE:  # type: ignore
+                        running = False
+                    if event.key == K_SPACE:  # type: ignore
+                        paused = True if not paused else False
+                if event.type == MOUSEBUTTONDOWN:  # type: ignore
+                    click_position = pygame.mouse.get_pos()
+                    pos_x, pos_y = (
+                        click_position[0] // self.cell_size,
+                        click_position[1] // self.cell_size,
+                    )
+                    if self.life.curr_generation[pos_x][pos_y]:
+                        self.life.curr_generation[pos_x][pos_y] = 0
+                    else:
+                        self.life.curr_generation[pos_x][pos_y] = 1
             self.draw_lines()
-            self.life.step()
             self.draw_grid()
+            if not paused:
+                self.life.step()
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
 
 
-# Uncomment to run game
-# if __name__ == "__main__":
-#     gui = GUI(GameOfLife((50, 50),  True, 10), 20)
-#     gui.run()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="GameOfLife")
+    parser.add_argument("--rows", type=int, default=20, help="Enter number of rows in grid")
+    parser.add_argument("--cols", type=int, default=20, help="Enter number of columns in grid")
+    parser.add_argument("--speed", type=int, default=10, help="Enter game speed")
+    parser.add_argument("--size", type=int, default=10, help="Enter cell size")
+    parser.add_argument(
+        "--primary_color", type=str, default="green", help="Enter color of alive cells"
+    )
+    parser.add_argument(
+        "--secondary_color", type=str, default="white", help="Enter color of dead cells"
+    )
+    parser.add_argument(
+        "--maxgenerations", type=int, default=50, help="Enter max number of generations"
+    )
+    parser.add_argument("--randomize", type=int, default=1, help="Should grid be randomized?")
+    arguments = parser.parse_args()
+    gui = GUI(
+        GameOfLife(
+            (arguments.rows, arguments.cols),
+            arguments.randomize,
+            arguments.maxgenerations,
+        ),
+        arguments.size,
+        arguments.speed,
+    )
+    gui.run()
