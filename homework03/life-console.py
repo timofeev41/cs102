b671c5efbb662118c8e9e5f44de53540f4ed7d30
Timofeev Nik <timofeevnik41@gmail.com>
2020-11-08 15:59:25 +0300

    Реализована консольная версия игры

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -10,13 +10,30 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border("|", "|", "-", "-", "#", "#", "#", "#")
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for pos_x, row in enumerate(self.life.curr_generation):
+            for pos_y, value in enumerate(row):
+                simbol = "@" if value else " "
+                try:
+                    screen.addstr(pos_x + 1, pos_y + 1, simbol)
+                except curses.error:
+                    continue
 
     def run(self) -> None:
-        screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        win = curses.initscr()
+        curses.curs_set(0)
+        win = curses.newwin(self.life.cols + 1, self.life.rows + 1, 0, 0)
+        self.life.curr_generation = self.life.create_grid(True)
+        running = True
+        while running:
+            if not self.life.is_changing or self.life.is_max_generations_exceeded:
+                running = False
+            win.clear()
+            self.draw_grid(win)
+            self.life.step()
+            self.draw_borders(win)
+            win.refresh()
         curses.endwin()
