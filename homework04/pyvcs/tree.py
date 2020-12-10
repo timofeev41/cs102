bafc0d17bce97f8fb1ea34c675f3b86512458241
timofeev41 <timofeevnik41@gmail.com>
2020-12-10 23:37:41 +0300

    Реализованы деревья

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -10,8 +10,36 @@ from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 
 
 def write_tree(gitdir: pathlib.Path, index: tp.List[GitIndexEntry], dirname: str = "") -> str:
-    # PUT YOUR CODE HERE
-    ...
+    tree_content: tp.List[tp.Tuple[int, pathlib.Path, bytes]] = []
+    subtrees: tp.Dict[str, tp.List[GitIndexEntry]] = dict()
+    files = []
+    for x in (gitdir.parent / dirname).glob("*"):
+        files.append(str(x))
+    for entry in index:
+        if entry.name in files:
+            tree_content.append((entry.mode, gitdir.parent / entry.name, entry.sha1))
+        else:
+            directory = entry.name.lstrip(dirname).split("/", 1)[0]
+            if directory not in subtrees:
+                subtrees[directory] = []
+            subtrees[directory].append(entry)
+    for tree in subtrees:
+        tree_content.append(
+            (
+                0o40000,
+                gitdir.parent / dirname / tree,
+                bytes.fromhex(
+                    write_tree(
+                        gitdir, subtrees[tree], dirname + "/" + tree if dirname != "" else tree
+                    )
+                ),
+            )
+        )
+    tree_content.sort(key=lambda _: _[1])
+    data = b"".join(
+        f"{elem[0]:o} {elem[1].name}".encode() + b"\00" + elem[2] for elem in tree_content
+    )
+    return hash_object(data, "tree", write=True)
 
 
 def commit_tree(
@@ -21,5 +49,16 @@ def commit_tree(
     parent: tp.Optional[str] = None,
     author: tp.Optional[str] = None,
 ) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    now = int(time.mktime(time.localtime()))
+    timezone = time.timezone
+    if timezone > 0:
+        tz_str = "." + f"{abs(timezone) // 60 // 60:02}{abs(timezone) // 60 % 60:02}"
+    else:
+        tz_str = "+" + f"{abs(timezone) // 60 // 60:02}{abs(timezone) // 60 % 60:02}"
+    cont = [f"tree {tree}"]
+    if parent is not None:
+        cont.append(f"parent {parent}")
+    cont.append(f"author {author} {now} {tz_str}")
+    cont.append(f"committer {author} {now} {tz_str}")
+    cont.append(f"\n{message}\n")
+    return hash_object("\n".join(cont).encode(), "commit", write=True)
