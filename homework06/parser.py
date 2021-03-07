8482f80e62b14130b36a4acadef98b912ad46d9e
Nikolas <timofeevnik41@gmail.com>
2021-03-07 12:27:30 +0300

    Исправление мелких недочетов

--- homework06/parser.py
+++ homework06/parser.py
@@ -1,27 +1,28 @@
+from typing import Any, Dict, List
+
 import requests
 from bs4 import BeautifulSoup
 
-from typing import Dict, List, Any
-
 News = List[Dict[str, str]]
+
 
 def extract_news(parser: BeautifulSoup) -> News:
     """ Extract news from a given web page """
     news_list: News = []
-    links = parser.select('.storylink')
-    subtext = parser.select('.subtext')
+    links = parser.select(".storylink")
+    subtext = parser.select(".subtext")
     for pos, item in enumerate(links):
         title = item.getText()
-        href = item.get('href', None)
-        points = subtext[pos].select('.score')[0].getText()
-        news_list.append({'title': title, 'link': href, 'points': points})
+        href = item.get("href", None)
+        points = subtext[pos].select(".score")[0].getText()
+        news_list.append({"title": title, "link": href, "points": points})
     return news_list
 
 
 def extract_next_page(parser: BeautifulSoup) -> str:
     """ Extract next page URL """
-    link = parser.select('.morelink')[0]['href']
-    return link[link.index('?'):]
+    link = parser.select(".morelink")[0]["href"]
+    return link[link.index("?") :]
 
 
 def get_news(url: str = "https://news.ycombinator.com/newest", n_pages: int = 1) -> News:
