4b99ef75cb776fe2eec146919c97dffaedad1a35
Nick <timofeevnik41@gmail.com>
2021-03-18 23:46:26 +0300

    Готовые тесты для db

--- homework06/tests/test_db.py
+++ homework06/tests/test_db.py
@@ -1,6 +1,16 @@
 import pytest
 from hackernews.database.db import *
 
+FAKE_NEWS = [
+    {"title": "Cats invaded Moon", "link": "example.com", "points": 12, "author": "Nikolas"},
+    {
+        "title": "Nikolas got 100 points for this task",
+        "link": "yandex.ru",
+        "points": 100,
+        "author": "Dementiy",
+    },
+]
+
 
 def db_set_up(engine):
     Base.metadata.create_all(bind=engine)
@@ -25,13 +35,44 @@ def session(engine):
     db_tear_down(session)
 
 
-def test_extract_all_news() -> None:
-    actual = extract_all_news_from_db()
-    s = session()
-    expected = s.query(News).all()
-    s.close()
-    assert len(expected) == len(actual)
+def test_news_can_be_saved(session):
+    add_news(session=session, news=FAKE_NEWS)
+
+    saved_item = session.query(News).get(1)
+    assert saved_item.title == FAKE_NEWS[0]["title"]
+    assert saved_item.author == FAKE_NEWS[0]["author"]
+
+    saved_item = session.query(News).get(2)
+    assert saved_item.title == FAKE_NEWS[1]["title"]
+    assert saved_item.author == FAKE_NEWS[1]["author"]
 
 
-def test_load_fresh_news() -> None:
-    pass
+def test_can_news_be_labeled(session):
+    add_news(session=session, news=FAKE_NEWS)
+
+    saved_item = session.query(News).get(1)
+    assert saved_item.label is None
+
+    label = "good"
+    update_label(session=session, id=1, label=label)
+    saved_item = session.query(News).get(1)
+    assert saved_item.label == label
+
+
+def test_all_news_can_be_extracted(session):
+    add_news(session=session, news=FAKE_NEWS)
+    news = extract_all_news_from_db(session=session)
+
+    assert len(news) == len(FAKE_NEWS)
+
+
+def test_fresh_news_can_be_added(session):
+    """Идея такая: у нас есть 2 новости, мы проверяем, что 30 новостей выгрузятся и запишутся рядом, не повредив
+    исходные данные"""
+    add_news(session=session, news=FAKE_NEWS)
+    news = extract_all_news_from_db(session=session)
+
+    load_fresh_news(session)
+
+    all_news = extract_all_news_from_db(session)
+    assert len(all_news) == 32
