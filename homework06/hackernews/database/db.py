72a5ae14ab3d302999d0311876ac8b011bc5181a
Nick <timofeevnik41@gmail.com>
2021-03-18 22:57:38 +0300

    Работа с дб намного проще

--- homework06/hackernews/database/db.py
+++ homework06/hackernews/database/db.py
@@ -68,5 +68,6 @@ Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     # При запуске db.py получаем 1000+ записей в базе данных
+    s = get_session(SessionLocal)
     news_list = get_news(n_pages=1)
-    add_news(news_list)
+    add_news(session=s, news=news_list)
