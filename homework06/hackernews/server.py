a1330bb676c643c6f523b561442afa6c9920d0c6
Nick <timofeevnik41@gmail.com>
2021-03-23 10:03:28 +0300

    Mypy декораторы

--- homework06/hackernews/server.py
+++ homework06/hackernews/server.py
@@ -1,3 +1,5 @@
+import typing as tp
+
 from bottle import route, run, template, redirect, request
 
 from hackernews.database.db import News, get_session, engine, update_label, load_fresh_news
@@ -5,12 +7,13 @@ from hackernews.database.db import News, get_session, engine, update_label, load
 
 # from bayes import NaiveBayesClassifier
 
-
+@tp.no_type_check
 @route("/")
 def main_page():
     return template("templates/main_menu")
 
 
+@tp.no_type_check
 @route("/news")
 def news_list():
     s = get_session(engine)
@@ -18,6 +21,7 @@ def news_list():
     return template("templates/news_template", rows=rows, more_button=True, label=True)
 
 
+@tp.no_type_check
 @route("/news_labeled")
 def news_list_labeled():
     s = get_session(engine)
@@ -27,6 +31,7 @@ def news_list_labeled():
     return template("templates/news_template", rows=rows, more_button=False, label=False)
 
 
+@tp.no_type_check
 @route("/add_label/")
 def add_label():
     s = get_session(engine)
@@ -35,21 +40,23 @@ def add_label():
         label, id = req.split("&")
     except ValueError:
         return template("templates/exception", exception="Expected 2 arguments. Label and ID.")
-    label = label[label.index("=") + 1 :]
-    id = id[id.index("=") + 1 :]
+    label = label[label.index("=") + 1:]
+    id = id[id.index("=") + 1:]
     update_label(session=s, id=id, label=label)
     redirect("/news")
 
 
+@tp.no_type_check
 @route("/update_news")
-def update_news():
+def update_news() -> None:
     s = get_session(engine)
     load_fresh_news(session=s)
     redirect("/news")
 
 
+@tp.no_type_check
 @route("/classify")
-def classify_news():
+def classify_news() -> None:
     pass
 
 
